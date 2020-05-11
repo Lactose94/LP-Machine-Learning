f1f61b18817fb409589987743fd94d97f8e9eb66
lactose94 <Julian.Kupka@gmx.net>
2020-05-11 16:22:41 +0200

    eddited out unused kernel function

--- kernel.py
+++ kernel.py
@@ -47,8 +47,8 @@ def linear_force_submat(q: np.array, config1: configuration, descriptors_array: 
 
     submat = np.zeros((n_ions * dim, nr_descriptors))
 
-    # IDEA: multiproccessing/ray to parallelize this loop -> interchange loops to have the 
-    # longer loop para. 
+    # IDEA: multiproccessing/ray to parallelize this loop -> interchange loops to have the
+    # longer loop para.
     # WARNING: Only the outer loop
     for l in range(nr_modi):
         # build the scalar prefactor for each distance vector
@@ -66,25 +66,25 @@ def gaussian_force_submat(q: np.array, config1: configuration, descriptors_array
     n_ions, modi_config = np.shape(config1.descriptors)
     _, dim = np.shape(config1.positions)
     nr_descriptors, modi_desc = np.shape(descriptors_array)
-    
+
     if not nr_modi == modi_config == modi_desc:
         raise ValueError('The nr of q\'s does not match')
-    
+
     submat = np.zeros((n_ions, dim, nr_descriptors))
-    
+
     for l in range(nr_modi):
         for k in range(n_ions):
             factor1 = grad_scalar(q[l], config1.nndistances[k])[:, np.newaxis] * config1.nndisplacements[k]
-            
+
             factor3a = gaussian_kernel(config1.get_nndescriptor(k), descriptors_array, sig)
             factor4a = descriptors_array[np.newaxis,:,l] - config1.get_nndescriptor(k)[:,l,np.newaxis]
             factor3b = gaussian_kernel(config1.descriptors[k,:], descriptors_array, sig)
             factor4b = descriptors_array[np.newaxis,:,l] - config1.descriptors[k,l,np.newaxis]
             factor5 = factor3a*factor4a + factor3b*factor4b
-            
+
             factor_all = np.sum(factor1[:, np.newaxis, :] * factor5[:, :, np.newaxis], axis=0)
             submat[k,:,:] += factor_all.T
-            
+
     submat = np.reshape(submat, (n_ions * dim, nr_descriptors))
     return -1/sig**2 * submat
 
@@ -101,14 +101,15 @@ class Kernel:
         else:
             raise ValueError(f'kernel {mode} is not supported')
 
+
     # builds a matrix-element for a given configuration
     # and !!one!! given descriptor vector (i.e. for !!one!! atom)
-    def energy_matrix_elements(self, descr1: np.array, descr2: np.array) -> np.array:
-        # TODO: check sum
-        # FIXME: this does not play well, if we want to completely flatten both input arrays.
-        sums = np.sum(self.kernel(descr1, descr2), axis=0)
-        return sums
+    #def energy_matrix_elements(self, descr1: np.array, descr2: np.array) -> np.array:
+    #    # TODO: check sum
+    #    # FIXME: this does not play well, if we want to completely flatten both input arrays.
+    #    sums = np.sum(self.kernel(descr1, descr2), axis=0)
+    #    return sums
 
     # applies the correct function to build the force submatrix
-    def force_submat(self, q: np.array, config1: configuration, config2: configuration) -> np.array:
-        return self.force_mat(q, config1, config2.descriptors)
+    #def force_submat(self, q: np.array, config1: configuration, config2: configuration) -> np.array:
+    #    return self.force_mat(q, config1, config2.descriptors)
