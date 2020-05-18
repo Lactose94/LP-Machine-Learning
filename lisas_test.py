3529644f98e7e89076c9b33e61ff98af1280dc1e
oOLisaOo <62755815+oOLisaOo@users.noreply.github.com>
2020-05-18 03:39:05 +0200

    Update lisas_test.py

--- lisas_test.py
+++ lisas_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import configuration as conf
 from time import time
-#from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge
 
 ##### Provided input data #####
 rcut = 4
@@ -46,7 +46,7 @@ C = np.empty([Ni*Na, Nq])
 C[0:Ni,:] = config1.descriptors
 C[Ni:2*Ni,:] = config2.descriptors
 E = np.array([[energy1],[energy2]])
-print(C)
+#print(C)
 
 ##### linear Kernel #####
 K1 = np.matmul(C,np.transpose(C)) # K[alpha', alpha, i', i] = K[alpha'*4+i', alpha*4+i]
@@ -59,28 +59,30 @@ for l in range(Ni*Na):
         K2[k,l] = np.sum(K1[k*Ni:(k+1)*Ni,l])
 #print(K2)
 X = np.matmul(np.transpose(K2),K2)
+#print(X)
 y = np.matmul(np.transpose(K2),E)
 lamb = 0.5
 # y = X*w + lamb*w     ->     w = ?
 # w = (X + lamb*I)^(-1) * y
 
 ##### Ridge Regression for energy
-"""
-# mit Paket
+
+### mit ridge-Paket
 start = time()
 clf = Ridge(alpha=lamb) # muss sklearn installieren
 clf.fit(X, y)
-w = clf.get_params()
+w = clf.coef_
 end = time()
 print("time passed for ridge packet:", end-start)
 print(w)
 """
+### mit 0815 numpy funktionen
 #start = time()
 w = np.matmul(np.linalg.inv(X + lamb * np.eye(Ni*Na)) , y)
 #end = time()
 #print("time passed for conservative inversion:", end-start)
 print(w)
-
+"""
 ##### Anwendung auf neue Konfiguration #####
 #positions3 = np.array([[2.0 , 2.0],
 #                       [2.0 , 8.0],
@@ -94,6 +96,15 @@ C_3 = config3.descriptors
 K1_3 = np.matmul(C_3,np.transpose(C))
 #print(K1_3)
 K2_3 = np.sum(K1_3, axis=0)
-#print(K2_3)
-E_3 = np.matmul(K2_3,w)
+print(K2_3)
+
+### mit 0815 numpy funktionen
+
+#coeffA = 1/np.tensordot(np.transpose(K2_3), K2_3, axes=1) * lamb + 1
+#E_3 = coeffA * np.matmul(w,K2_3.reshape(-1, 1))
+E_3 = np.matmul(w,K2_3.reshape(-1, 1))
+print(E_3)
+
+### mit ridge-Paket
+E_3 = clf.predict(K2_3.reshape(1, -1))
 print(E_3)
