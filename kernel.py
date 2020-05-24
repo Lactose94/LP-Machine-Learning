45a9dff01272488287555e501d4c1b1d50a8925a
oOLisaOo <62755815+oOLisaOo@users.noreply.github.com>
2020-05-24 20:06:08 +0200

    Mega-Update Gauss Kernel
    + Quickest Gauss-Kernel that I could build
    + New prediction test file to see first results (energies are good but forces not so much)
    + corrected a bunch of errors that came to light due to prediction results

--- kernel.py
+++ kernel.py
@@ -13,16 +13,22 @@ def linear_kernel(descr_list1: np.array, descr_list2: np.array) -> np.array:
     
     return np.matmul(descr_list1, descr_list2.T) # it says in the documentation that matmul is preferred over dot
 
-
+# descr_list1 ist die aktuelle Konfiguration, descr_list2 die Referenz-Konfiguration!
 def gaussian_kernel(descr_list1: np.array, descr_list2: np.array, sigma: float) -> np.array:
-    descr_diff = np.transpose(np.diagonal(np.subtract.outer(descr_list2, descr_list1), axis1=3, axis2=1), axes=(1,0,2))
-    # Kernel-Matrix: dim = [ Nj*Nb , Ni*Na ]
-    kernel = np.sum(descr_diff**2, axis=2)
-    kernel = np.exp(- kernel / (2 * sigma**2))
     
-    return kernel
+    nbnj, nq = np.shape(descr_list1)
+    nani, _ = np.shape(descr_list2)
+    
+    abs1 = np.sum(descr_list1 ** 2, axis=1)
+    abs2 = np.sum(descr_list2 ** 2, axis=1)
+    
+    coeffs = linear_kernel(descr_list1, descr_list2)
+    
+    dr = abs1.reshape(nbnj, 1) - 2 * coeffs + abs2.reshape(1, nani)
+    
+    return np.exp(-dr / (2 * sigma**2))
 
-
+"""
 def derivatives(q: np.array, displ: np.array, dist: np.array) -> np:
     '''
     Calculate the derivatives matrix of the descriptors
@@ -35,7 +41,7 @@ def derivatives(q: np.array, displ: np.array, dist: np.array) -> np:
     D = qcosrq.reshape(nc, ni, ni, 1, nq) *  R_over_r.reshape(nc, ni, ni, d, 1)
 
     return D
-
+"""
 
 # builds part of the row for the force kernel matrix given a configuration and a set of descriptors
 def linear_force_submat(q: np.array, config1: configuration, descriptors_array: np.array) -> np.array:
@@ -63,43 +69,57 @@ def linear_force_submat(q: np.array, config1: configuration, descriptors_array: 
 
     return -2 * submat
 
-def gaussian_force_mat(q: np.array, config1: configuration, descr_diff: np.array, sig: float) -> np.array: # kernel_matrix:np.array muss auch irgendwie übergeben werden...
-    nr_modi = len(q)
-    n_ions, modi_config = np.shape(config1.descriptors)
+# builds part of the row for the force kernel matrix given a configuration and a set of descriptors
+def gaussian_force_mat(q: np.array, config1: configuration, descriptors_array: np.array, sigma: float) -> np.array:
+    nq = len(q)
+    nj, modi_config = np.shape(config1.descriptors)
     _, dim = np.shape(config1.positions)
-    nr_descriptors, modi_desc = np.shape(descr_diff)
+    nani, modi_desc = np.shape(descriptors_array)
     
-    if not nr_modi == modi_config == modi_desc:
+    if not nq == modi_config == modi_desc:
         raise ValueError('The nr of q\'s does not match')
     
-    submat = np.zeros((n_ions, dim, nr_descriptors))
+    dist = config1.nndistances
+    # R_over_r.shape = (nj', ni', dim)
+    R_over_r = config1.nndisplace_norm
     
-    #for l in range(nr_modi):
-    #    for k in range(n_ions):
-    #        factor1 = grad_scalar(q[l], config1.nndistances[k])[:, np.newaxis] * config1.nndisplacements[k]
-    #
-    #        factor3a = gaussian_kernel(config1.get_nndescriptor(k), descriptors_array, sig)
-    #        factor4a = descriptors_array[np.newaxis,:,l] - config1.get_nndescriptor(k)[:,l,np.newaxis]
-    #        factor3b = gaussian_kernel(config1.descriptors[k,:], descriptors_array, sig)
-    #        factor4b = descriptors_array[np.newaxis,:,l] - config1.descriptors[k,l,np.newaxis]
-    #        factor5 = factor3a*factor4a + factor3b*factor4b
-    #
-    #        factor_all = np.sum(factor1[:, np.newaxis, :] * factor5[:, :, np.newaxis], axis=0)
-    #        submat[k,:,:] += factor_all.T
+    rq = dist.reshape(nj,nj,1) * q.reshape(1,1,nq)
+    # cosrq.shape = (nj', ni', nq)
+    cosrq = np.cos(rq)
     
-    submat = np.reshape(submat, (n_ions * dim, nr_descriptors))
-    return -1/sig**2 * submat
+    # kern.shape = (nj, nani)
+    kern = gaussian_kernel(config1.descriptors, descriptors_array, sigma)
+    
+    q_sig = (-1/(sigma**2) * q)
+    # q_sig_Cia.shape = (nani, nq)
+    q_sig_Cia = q_sig.reshape(1,nq) * descriptors_array
+    # q_sig_Cj1.shape = (nj, nq)
+    q_sig_Cj1 = q_sig.reshape(1,nq) * config1.descriptors
+    # cosrq_Ror.shape = (nj', ni', dim, nq)
+    cosrq_Ror = cosrq.reshape(nj,nj,1,nq) * R_over_r.reshape(nj,nj,dim,1)
+    # sumi_cosrq_Ror.shape = (nj', dim, nq)
+    sumi_cosrq_Ror = np.sum(cosrq_Ror, axis=1)
+    
+    # M_C.shape = (nj', dim, nani)
+    M_Ciai1 = np.sum(np.sum(cosrq_Ror.reshape(nj,nj,dim,1,nq) * kern.reshape(1,nj,1,nani,1), axis=1) * q_sig_Cia.reshape(1,1,nani,nq), axis=3)
+    M_Ci1i1 = np.sum(((np.sum(cosrq * q_sig_Cj1.reshape(1,nj,nq), axis=2)).reshape(nj,nj,1) * R_over_r).reshape(nj,nj,dim,1) * kern.reshape(1,nj,1,nani), axis=1)
+    M_Ciaj1 = np.sum(sumi_cosrq_Ror.reshape(nj,dim,1,nq) * q_sig_Cia.reshape(1,1,nani,nq), axis=3) * kern.reshape(nj,1,nani)
+    M_Cj1j1 = (np.sum(sumi_cosrq_Ror * q_sig_Cj1.reshape(nj,1,nq), axis=2)).reshape(nj,dim,1) * kern.reshape(nj,1,nani)
+    
+    submat = (M_Ciai1 - M_Ci1i1 + M_Ciaj1 - M_Cj1j1).reshape(nj*dim,nani)
+    
+    return submat
 
 class Kernel:
     def __init__(self, mode, *args):
         if mode == 'linear':
-            self.kernel = linear_kernel
-            self.force_mat = linear_force_submat
+            self.kernel_mat = linear_kernel
+            self.force_submat = linear_force_submat
         elif mode == 'gaussian':
             if not args:
                 raise ValueError('For the Gaussian Kernel a sigma has to be supplied')
-            self.kernel = lambda x, y: gaussian_kernel(x, y, args[0])
-            self.force_mat = lambda x, y, z: gaussian_force_mat(x, y, z, args[0]) # Generell neu schreiben, self.descr_diff an force_submat übergeben
+            self.kernel_mat = lambda x, y: gaussian_kernel(x, y, args[0])
+            self.force_submat = lambda x, y, z: gaussian_force_mat(x, y, z, args[0])
         else:
             raise ValueError(f'kernel {mode} is not supported')
     
