1ece55ae472c9a3a365a39788d28666d3e4d1d4d
lactose94 <Julian.Kupka@gmx.net>
2020-06-05 10:25:27 +0200

    Added offset to outcar parser to improve data loading.

--- outcar_parser.py
+++ outcar_parser.py
@@ -19,15 +19,15 @@ class Parser:
         self.filepath = filepath
         if not search(r'outcar\.\d+', self.filepath, IGNORECASE):
             raise ValueError(f'expected outcar file, got {self.filepath}')
-            
+
         with open(self.filepath, 'r') as outcar_in:
             self.outcar_content = outcar_in.read()
-        
+
     # Durchsucht den Inhalt nach dem oben angegebenen Muster, sprich nach der Zeile, in der die Ionen stehen
     # Falls die entsprechende Zeile nicht auffindbar ist, bricht das Programm ab
     def find_ion_nr(self) -> int:
-        ion_match = search(ION_PATTERN, 
-                            self.outcar_content, 
+        ion_match = search(ION_PATTERN,
+                            self.outcar_content,
                             IGNORECASE)
         if not ion_match:
             raise RuntimeError(f'could not find match for ions')
@@ -39,12 +39,12 @@ class Parser:
     # Falls die entsprechende Zeile nicht auffindbar ist, bricht das Programm ab
     def find_lattice_vectors(self) -> array:
         lattice_match = search(LATTICE_PATTERN,
-                                self.outcar_content, 
+                                self.outcar_content,
                                 IGNORECASE)
 
         if not lattice_match:
             raise RuntimeError(f'could not find match for lattice vectors')
-            
+
         matches = lattice_match.groups()
         lattice_string = list(map(lambda vec_string: vec_string.strip().split(), matches))
         lattice_float = list(map(lambda list_vecs: self.__convert_list(list_vecs), lattice_string))
@@ -55,10 +55,10 @@ class Parser:
     # Teilt den Inhalt erst in Konfigurationen und findet die Energien, Positionen sowie Kräfte
     # und baut daraus einen Iterator
     # IDEA: normalize data by subtracting the mean of the enregies
-    def build_configurations(self, step_size: int) -> (float, array, array):
+    def build_configurations(self, step_size: int, offset=0) -> (float, array, array):
         # Teilt den Inhalt an den in SPLIT_CONFIGS angegebenen Zeilen, die recht zuverlässig die einzelnen
         # Konfigurationen trennen sollten, wählt anschließend jede step_size-te Konfiguration aus
-        configs = self.outcar_content.split(SPLIT_CONFIGS)[1::step_size]
+        configs = self.outcar_content.split(SPLIT_CONFIGS)[1+offset::step_size]
         for (i, config) in enumerate(configs):
             # Sucht nach der Zeile, die die Energie enthalten sollte. Falls diese nicht gefunden werden kann, abbruch
             energy_match = search(ENERGY_PATTERN, config, IGNORECASE)
@@ -81,18 +81,18 @@ class Parser:
                 yield (energy, positions, forces)
 
     # private Hilfsfunktion die Listen von str in Listen von floats umwandelt
-    def __convert_list(self, val_list) -> list: 
+    def __convert_list(self, val_list) -> list:
         return list(map(lambda entry: float(entry.strip()), val_list))
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     test_in = 'OUTCAR.21'
 
     test_lattice = array([
-                        [10.546640000 , 0.000000000,  0.000000000], 
-                        [0.000000000, 10.546640000,  0.000000000], 
+                        [10.546640000 , 0.000000000,  0.000000000],
+                        [0.000000000, 10.546640000,  0.000000000],
                         [0.000000000,  0.000000000, 10.546640000]
                         ])
-            
+
     parser = Parser(test_in)
     nr_ions = parser.find_ion_nr()
     assert type(nr_ions) == int, f'nr of ions should be integer, is {type(nr_ions)}'
